8ce1775579a2b011eb367c1e84fd587b26ce5985
Thiago Santos <virtualves@gmail.com>
2022-01-14 09:08:21 -0400

    Implementando toString pedido, apos envio para producao

---
+++ src/test/java/com/thiago/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,14 @@
+package com.thiago.cursomc;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
