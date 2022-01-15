466f608ccc8543ba9f60e0be79b3f5cbed89f3c0
Thiago Santos <virtualves@gmail.com>
2022-01-15 10:49:32 -0400

    Esqueci a senha

--- src/main/java/com/thiago/cursomc/services/EmailService.java
+++ src/main/java/com/thiago/cursomc/services/EmailService.java
@@ -2,6 +2,7 @@ package com.thiago.cursomc.services;
 
 import org.springframework.mail.SimpleMailMessage;
 
+import com.thiago.cursomc.domain.Cliente;
 import com.thiago.cursomc.domain.Pedido;
 
 public interface EmailService {
@@ -10,4 +11,6 @@ public interface EmailService {
 	void sendOrderConfirmationEmail(Pedido obj);
 	
 	void sendEmail(SimpleMailMessage msg);
+	
+	void sendNewPasswordEmail(Cliente cliente, String newPass);
 }
